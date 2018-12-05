265e65cfec5ab66fc4b3841cf58ac200ffb43343
William Kristiansen <wjkristiansen@hotmail.com>
2018-12-05 13:08:28 -0800

    Going back to enum class InterfaceId

--- src/CanvasCore/Scene.h
+++ src/CanvasCore/Scene.h
@@ -18,7 +18,7 @@ public:
 
     CANVASMETHOD(InternalQueryInterface)(InterfaceId iid, void **ppUnk)
     {
-        if (iid == IId_XSceneGraphNode)
+        if (iid == InterfaceId::IId_XSceneGraphNode)
         {
             *ppUnk = this;
             return Result::Success;
@@ -75,7 +75,7 @@ public:
 
     CANVASMETHOD(InternalQueryInterface)(InterfaceId iid, _Outptr_ void **ppObj)
     {
-        if (IId_XScene == iid)
+        if (InterfaceId::IId_XScene == iid)
         {
             *ppObj = this;
             AddRef();
