d73a2de6d3d860e8288a08c0e438bfe84d428cec
William Kristiansen <wjkristiansen@hotmail.com>
2018-12-09 21:07:19 -0800

    Tree iterators don't make sense... to me... yet

--- src/CanvasCore/Scene.cpp
+++ src/CanvasCore/Scene.cpp
@@ -6,85 +6,6 @@
 
 using namespace Canvas;
 
-template<class _NodeType>
-class CTreeIterator
-{
-public:
-    std::deque<_NodeType *> m_Stack;
-
-    CANVASMETHOD(MoveNext)()
-    {
-        return Result::NotImplemented;
-    }
-    CANVASMETHOD(MovePrev)()
-    {
-        return Result::NotImplemented;
-    }
-};
-
-//------------------------------------------------------------------------------------------------
-class CSceneGraphIterator :
-    public XTreeIterator,
-    public CGenericBase
-{
-public:
-    CSceneGraphNode *m_pCurrent; // weak-ptr
-
-    CSceneGraphIterator(_In_ CSceneGraphNode *pNode) :
-        m_pCurrent(pNode) 
-    {
-    }
-
-    CANVASMETHOD(MoveNext)() final
-    {
-        if (m_pCurrent->m_pNextSibling)
-        {
-            m_pCurrent = m_pCurrent->m_pNextSibling;
-            return Result::Success;
-        }
-
-        return Result::End;
-    }
-
-    CANVASMETHOD(MovePrev)() final
-    {
-        if (m_pCurrent->m_pPrevSibling)
-        {
-            m_pCurrent = m_pCurrent->m_pPrevSibling;
-            return Result::Success;
-        }
-
-        return Result::End;
-    }
-
-    CANVASMETHOD(MoveParent)() final
-    {
-        if (m_pCurrent->m_pParent)
-        {
-            m_pCurrent = m_pCurrent->m_pParent;
-            return Result::Success;
-        }
-
-        return Result::End;
-    }
-
-    CANVASMETHOD(MoveFirstChild)() final
-    {
-        if (m_pCurrent->m_pFirstChild)
-        {
-            m_pCurrent = m_pCurrent->m_pFirstChild;
-            return Result::Success;
-        }
-
-        return Result::End;
-    }
-
-    CANVASMETHOD(Select)(InterfaceId iid, _Outptr_ void **ppObj)
-    {
-        return m_pCurrent->QueryInterface(iid, ppObj);
-    }
-};
-
 //------------------------------------------------------------------------------------------------
 CANVASMETHODIMP CSceneGraphNode::InternalQueryInterface(InterfaceId iid, void **ppUnk)
 {
@@ -109,31 +30,6 @@ CANVASMETHODIMP CSceneGraphNode::AddChild(_In_ XSceneGraphNode *pChild)
 
     return Result::Success;
 }
-
-//------------------------------------------------------------------------------------------------
-CANVASMETHODIMP CSceneGraphNode::MakeIterator(_Inout_ XTreeIterator **ppIterator)
-{
-    *ppIterator = nullptr;
-
-    if (!ppIterator)
-    {
-        return Result::BadPointer;
-    }
-
-    try
-    {
-        CSceneGraphIterator *pIterator = new CGeneric<CSceneGraphIterator>(this); // throw(std::bad_alloc)
-        pIterator->AddRef();
-        *ppIterator = pIterator;
-    }
-    catch (std::bad_alloc &)
-    {
-        return Result::OutOfMemory;
-    }
-
-    return Result::Success;
-}
-
 
 //------------------------------------------------------------------------------------------------
 CANVASMETHODIMP CSceneGraphNode::Prune()
