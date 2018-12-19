6814c856d5891d48e2f8e042f9f345d56956496e
William Kristiansen <wjkristiansen@hotmail.com>
2018-12-18 21:50:16 -0800

    Stub graphics base classes

--- src/CanvasCore/GraphicsDevice.h
+++ src/CanvasCore/GraphicsDevice.h
@@ -7,10 +7,74 @@
 namespace Canvas
 {
 
+//------------------------------------------------------------------------------------------------
+// Abstract graphics device class
 class CGraphicsDevice :
     public TGeneric<CGenericBase>
 {
 
 };
+
+//------------------------------------------------------------------------------------------------
+// Submits command streams to the GPU.
+// Manages synchronization with other command contexts and
+// the CPU.
+class CCommandContext :
+    public TGeneric<CGenericBase>
+{
+
+};
+
+
+//------------------------------------------------------------------------------------------------
+// Batches up work for submission to the GPU
+class CCommandStream :
+    public TGeneric<CGenericBase>
+{
+
+};
+
+//------------------------------------------------------------------------------------------------
+// Buffers that are CPU-writable and GPU readable.
+// Typically used for uploading data.  May also be used for constant
+// data, vertex buffers or index buffers depending on access frequency.
+// GPU access to these is not as fast as CBuffer.
+class CUploadBuffer :
+    public TGeneric<CGenericBase>
+{
+
+};
+
+//------------------------------------------------------------------------------------------------
+// Buffers that are GPU accessible only.  Typically
+// GPU reads from CBuffer data is faster than CUploadBuffer data.
+// CBuffers are stateful.
+class CBuffer :
+    public TGeneric<CGenericBase>
+{
+
+};
+
+//------------------------------------------------------------------------------------------------
+class CTexture :
+    public TGeneric<CGenericBase>
+{
+
+};
+
+//------------------------------------------------------------------------------------------------
+class CPipelineState :
+    public TGeneric<CGenericBase>
+{
+
+};
+
+//------------------------------------------------------------------------------------------------
+class CDescriptorHeap :
+    public TGeneric<CGenericBase>
+{
+
+};
+
 
 }
