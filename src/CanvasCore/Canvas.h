966350dd4e6196c8c34db79e62e3fdeb91d44fde
William Kristiansen <wjkristiansen@hotmail.com>
2019-02-09 00:12:31 -0800

    XCanvas inherits from XLogger and can override LogOutputProc

--- src/CanvasCore/Canvas.h
+++ src/CanvasCore/Canvas.h
@@ -35,14 +35,39 @@ inline Result HResultToResult(HRESULT hr)
 }
 
 //------------------------------------------------------------------------------------------------
+class CLogger : public XCanvas
+{
+    LOG_OUTPUT_LEVEL m_MaxLogOutput;
+    LogOutputProc m_LogOutputProc;
+
+public:
+    CLogger(LogOutputProc OutputProc) :
+        m_MaxLogOutput(LOG_OUTPUT_LEVEL_MESSAGE),
+        m_LogOutputProc(OutputProc)
+    {
+        if (m_LogOutputProc == nullptr)
+        {
+            m_LogOutputProc = DefaultOutputProc;
+        }
+    }
+
+    static void DefaultOutputProc(LOG_OUTPUT_LEVEL Level, PCWSTR szSTring);
+    
+    // XLogger methods
+    GEMMETHOD_(void, SetMaxOutputLevel)(LOG_OUTPUT_LEVEL Level) { m_MaxLogOutput = Level; }
+    GEMMETHOD_(void, WriteToLog)(LOG_OUTPUT_LEVEL Level, PCWSTR szString);
+    GEMMETHOD_(void, SetLogOutputProc)(LogOutputProc OutputProc) { m_LogOutputProc = OutputProc; }
+};
+
+//------------------------------------------------------------------------------------------------
 class CCanvas :
-    public XCanvas,
+    public CLogger,
     public CGenericBase
 {
 public:
-    CCanvas(CLogger *pLogger) :
+    CCanvas(LogOutputProc OutputProc) :
         CGenericBase(),
-        m_pLogger(pLogger)
+        CLogger(OutputProc)
     {}
 
     ~CCanvas();
@@ -50,7 +75,6 @@ public:
     std::map<std::wstring, CObjectName *> m_ObjectNames;
     struct Sentinel {};
     TAutoList<TStaticPtr<CCanvasObjectBase>> m_OutstandingObjects;
-    CLogger *m_pLogger = nullptr;
 
     GEMMETHOD(GetNamedObject)(_In_z_ PCWSTR szName, Gem::InterfaceId iid, _Outptr_ void **ppObj)
     {
@@ -62,6 +86,7 @@ public:
         return Result::NotFound;
     }
 
+    // XCanvas methods
     GEMMETHOD(InternalQueryInterface)(Gem::InterfaceId iid, _Outptr_ void **ppObj);
     GEMMETHOD(CreateScene)(Gem::InterfaceId iid, _Outptr_ void **ppObj) final;
     GEMMETHOD(CreateSceneGraphNode)(Gem::InterfaceId iid, _Outptr_ void **ppObj, PCWSTR szName = nullptr) final;
@@ -72,8 +97,6 @@ public:
     Result SetupD3D12(CANVAS_GRAPHICS_OPTIONS *pGraphicsOptions, HWND hWnd, _Outptr_opt_ XGraphicsDevice **ppGraphicsDevice);
 
     void ReportObjectLeaks();
-
-    CLogger *GetLogger() const { return m_pLogger; }
 
 public:
     TGemPtr<class CGraphicsDevice> m_pGraphicsDevice;
