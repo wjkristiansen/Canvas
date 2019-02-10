5684eae9cb11d0164157169c11961057365621ea
Bill <wjkristiansen@hotmail.com>
2019-02-09 21:09:45 -0800

    Adds some log output

--- src/CanvasCore/CanvasCore.cpp
+++ src/CanvasCore/CanvasCore.cpp
@@ -10,9 +10,19 @@ using namespace Canvas;
 //------------------------------------------------------------------------------------------------
 GEMMETHODIMP_(void) CLogger::WriteToLog(LOG_OUTPUT_LEVEL Level, PCWSTR szString)
 {
+    static PCWSTR PrefixStrings[] =
+    {
+        L"CANVAS ERROR: ", // LOG_OUTPUT_LEVEL_ERROR
+        L"CANVAS WARNING: ", // LOG_OUTPUT_LEVEL_WARNING
+        L"CANVAS MESSAGE: ", // LOG_OUTPUT_LEVEL_MESSAGE
+        L"CANVAS: ", // LOG_OUTPUT_LEVEL_VERBOSE
+    };
+
     if (Level <= m_MaxLogOutput)
     {
-        m_LogOutputProc(Level, szString);
+        std::wostringstream ostr;
+        ostr << PrefixStrings[Level] << szString;
+        m_LogOutputProc(Level, ostr.str().c_str());
     }
 }
 
@@ -69,6 +79,7 @@ GEMMETHODIMP CCanvas::CreateScene(InterfaceId iid, _Outptr_ void **ppObj)
     }
     catch(std::bad_alloc &)
     {
+        WriteToLog(LOG_OUTPUT_LEVEL_ERROR, L"Out of memory XCanvas::CreateScene");
         return Result::OutOfMemory;
     }
 }
@@ -83,6 +94,7 @@ GEMMETHODIMP CCanvas::CreateSceneGraphNode(InterfaceId iid, _Outptr_ void **ppOb
     }
     catch(std::bad_alloc &)
     {
+        WriteToLog(LOG_OUTPUT_LEVEL_ERROR, L"Out of memory XCanvas::CreateSceneGraphNode");
         return Result::OutOfMemory;
     }
 }
@@ -92,6 +104,7 @@ void CCanvas::ReportObjectLeaks()
 {
     for (auto pNode = m_OutstandingObjects.GetFirst(); pNode != m_OutstandingObjects.GetEnd(); pNode = pNode->GetNext())
     {
+        std::wostringstream ostr;
         CCanvasObjectBase *pObject = pNode->Ptr();
 
         std::wcout << L"Leaked object: ";
@@ -100,15 +113,16 @@ void CCanvas::ReportObjectLeaks()
         if (Succeeded(pObject->InternalQueryInterface(GEM_IID_PPV_ARGS(&pObjectName))))
         {
             pObjectName->Release();
-            std::wcout << L"Name=\"" << pObjectName->GetName() << L"\", ";
+            ostr << L"Name=\"" << pObjectName->GetName() << L"\", ";
         }
         XGeneric *pXGeneric;
         if (Succeeded(pObject->InternalQueryInterface(GEM_IID_PPV_ARGS(&pXGeneric))))
         {
             ULONG RefCount = pXGeneric->Release();
-            std::wcout << L"RefCount=" << RefCount;
+            ostr << L"RefCount=" << RefCount;
         }
-        std::wcout << std::endl;
+        
+        WriteToLog(LOG_OUTPUT_LEVEL_ERROR, ostr.str().c_str());
     }
 }
 
@@ -116,27 +130,26 @@ void CCanvas::ReportObjectLeaks()
 Result GEMAPI CreateCanvas(InterfaceId iid, void **ppCanvas, LogOutputProc OutputProc)
 {
     *ppCanvas = nullptr;
-    //static CDefaultLogger DefaultLogger;
-
-    //if (nullptr == pLogger)
-    //{
-    //    pLogger = &DefaultLogger;
-    //}
 
     try
     {
         if (iid == XCanvas::IId)
         {
             TGemPtr<XCanvas> pCanvas = new TGeneric<CCanvas>(OutputProc); // throw(bad_alloc)
+            pCanvas->WriteToLog(LOG_OUTPUT_LEVEL_MESSAGE, L"CreateCanvas");
             return pCanvas->QueryInterface(iid, ppCanvas);
         }
     }
     catch (std::bad_alloc &)
     {
+        if (OutputProc)
+        {
+            OutputProc(LOG_OUTPUT_LEVEL_ERROR, L"FAILURE in CreateCanvas");
+        }
         return Result::OutOfMemory;
     }
 
-    return Result::Success;
+    return Result::NoInterface;
 }
 
 //------------------------------------------------------------------------------------------------
@@ -161,6 +174,7 @@ GEMMETHODIMP CCanvas::CreateGraphicsDevice(CANVAS_GRAPHICS_OPTIONS *pGraphicsOpt
 GEMMETHODIMP CCanvas::FrameTick()
 {
     Result result = Result::Success;
+    WriteToLog(LOG_OUTPUT_LEVEL_VERBOSE, L"Begin CCanvas::FrameTick");
 
     // Elapse time
 
@@ -171,6 +185,8 @@ GEMMETHODIMP CCanvas::FrameTick()
     // Render the display list
 
     m_pGraphicsDevice->RenderFrame();
+
+    WriteToLog(LOG_OUTPUT_LEVEL_VERBOSE, L"End CCanvas::FrameTick");
 
     return result;
 }
@@ -186,6 +202,7 @@ Result CCanvas::SetupD3D12(CANVAS_GRAPHICS_OPTIONS *pGraphicsOptions, HWND hWnd,
     TGemPtr<CGraphicsDevice> pGraphicsDevice;
     try
     {
+        WriteToLog(LOG_OUTPUT_LEVEL_MESSAGE, L"CCanvas::SetupD3D12");
         ThrowGemError(CreateGraphicsDevice12(this, &pGraphicsDevice, hWnd));
 
         if (ppGraphicsDevice)
@@ -196,6 +213,7 @@ Result CCanvas::SetupD3D12(CANVAS_GRAPHICS_OPTIONS *pGraphicsOptions, HWND hWnd,
     }
     catch (GemError &gomError)
     {
+        WriteToLog(LOG_OUTPUT_LEVEL_ERROR, L"ERROR in CCanvas::SetupD3D12");
         return gomError.Result();
     }
 
--- src/CanvasCore/stdafx.h
+++ src/CanvasCore/stdafx.h
@@ -22,6 +22,7 @@
 #include <unordered_set>
 #include <unordered_map>
 #include <iostream>
+#include <sstream>
 
 // reference additional headers your program requires here
 #include <fbxsdk.h>
