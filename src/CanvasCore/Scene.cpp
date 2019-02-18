e4c129111d8070e9d25439321007d9ca212c53ed
Bill <wjkristiansen@hotmail.com>
2019-02-18 13:17:55 -0800

    More logging added

--- src/CanvasCore/Scene.cpp
+++ src/CanvasCore/Scene.cpp
@@ -75,6 +75,7 @@ GEMMETHODIMP CSceneGraphNode::CreateChildIterator(_Outptr_ XIterator **ppIterato
     }
     catch (std::bad_alloc &)
     {
+        m_pCanvas->Logger().LogError(L"Out of memory: CSceneGraphNode::CreateChildIterator");
         return Result::OutOfMemory;
     }
 }
