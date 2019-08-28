4b68c69182c17e07ea661feda31c857275e79ccb
William Kristiansen <wjkristiansen@hotmail.com>
2019-08-27 22:37:53 -0700

    Powershell script for installing FBX SDK from the internet

---
+++ scripts/InstallFBX.ps1
@@ -0,0 +1,11 @@
+﻿#install the FBX SDK
+$fbx_sdk_url="https://www.autodesk.com/content/dam/autodesk/www/adn/fbx/20192/fbx20192_fbxsdk_vs2017_win.exe"
+$fbx_install_dir="c:\fbxsdx"
+Invoke-WebRequest -Uri $fbx_sdk_url -OutFile ./fbxinstall.exe
+.\fbxinstall.exe /S /D=$fbx_install_dir
+
+# Set the environment variables for FBX
+$fbx_inc_dir=$fbx_install_dir+"\include"
+$fbx_lib_dir=$fbx_install_dir+"\lib\vs2017"
+[System.Environment]::SetEnvironmentVariable("FBX_SDK_INC_DIR", $fbx_inc_dir, [System.EnvironmentVariableTarget]::User)
+[System.Environment]::SetEnvironmentVariable("FBX_SDK_LIB_DIR", $fbx_lib_dir, [System.EnvironmentVariableTarget]::User)
