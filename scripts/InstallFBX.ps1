6a8769be7a1c6b2a81fcf92206c2d1b5e478295c
William Kristiansen <wjkristiansen@hotmail.com>
2019-08-30 15:50:53 +0000

    Updated InstallFBX.ps1

--- scripts/InstallFBX.ps1
+++ scripts/InstallFBX.ps1
@@ -1,10 +1,15 @@
 ﻿#install the FBX SDK
+echo "Downloading FBX SDK..."
 $fbx_sdk_url="https://www.autodesk.com/content/dam/autodesk/www/adn/fbx/20192/fbx20192_fbxsdk_vs2017_win.exe"
-$fbx_install_dir="c:\fbxsdx"
 Invoke-WebRequest -Uri $fbx_sdk_url -OutFile ./fbxinstall.exe
+
+echo "Installing FBX SDK..."
+$fbx_install_dir="c:\fbxsdx"
 .\fbxinstall.exe /S /D=$fbx_install_dir
+dir $fbx_install_dir
 
 # Set the environment variables for FBX
+echo "Setting FBX_SDK_* environment variables"
 $fbx_inc_dir=$fbx_install_dir+"\include"
 $fbx_lib_dir=$fbx_install_dir+"\lib\vs2017"
 [System.Environment]::SetEnvironmentVariable("FBX_SDK_INC_DIR", $fbx_inc_dir, [System.EnvironmentVariableTarget]::User)
